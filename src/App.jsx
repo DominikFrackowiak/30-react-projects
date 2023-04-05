cf243a2419973e93a8b9583adda9f56e160cadb7
DominikFrackowiak <rorschach@op.pl>
2023-04-05 09:48:05 +0200

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
+import RandomizeColors from './projects/02-randomize-colors/RandomizeColors'
 
-import './App.css'
+import '../App.css'
 
 function App() {
-  
-
-  return (
-    <div className="App">
-      
-    </div>
-  )
+	return (
+		<div className='App'>
+			<RandomizeColors/>
+		</div>
+	)
 }
 
 export default App
